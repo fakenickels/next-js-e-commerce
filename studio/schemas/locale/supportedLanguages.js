042dac6264bcf9d478e15bfcada278543d9e70d7
Gabriel Rubens <grsabreu@gmail.com>
2020-11-06 13:38:19 -0300

    Update supportedLanguages.js

--- studio/schemas/locale/supportedLanguages.js
+++ studio/schemas/locale/supportedLanguages.js
@@ -1,5 +1,5 @@
 export default [
-  { id: "pt-BR", title: "Português", isDefault: true },
+  { id: "pt_BR", title: "Português", isDefault: true },
   { id: "en", title: "Portuguese" },
   { id: "nb", title: "Norwegian" },
 ];
